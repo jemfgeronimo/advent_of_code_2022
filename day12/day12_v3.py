71841f5be659970ac18aa5ed7f8c399a84360ca4
Jem Geronimo <Johnerasmusmari.Geronimo@analog.com>
2022-12-14 13:02:04 +0800

    day 12 v 3: height map parsed from input
    Signed-off-by: Jem Geronimo <Johnerasmusmari.Geronimo@analog.com>

--- day12/day12_v3.py
+++ day12/day12_v3.py
@@ -3,4 +3,26 @@ if SAMPLE_INPUT_ON:
     f = open("sample.txt", "r")
 else:
     f = open("input.txt", "r")
+
+hmap = []
+for line in f:
+    hmap.append([ord(ch) for ch in line.strip()])
+    #print(line)
+
+    for i in hmap[-1]:
+        if i == ord("S"):
+            S_xcorr = hmap[-1].index(i)
+            S_ycorr = len(hmap) - 1
+            hmap[S_ycorr][S_xcorr] = ord("a")
+        elif i == ord("E"):
+            E_xcorr = hmap[-1].index(i)
+            E_ycorr = len(hmap) - 1
+            hmap[S_ycorr][S_xcorr] = ord("z")
 f.close()
+
+#for i in hmap:
+#    print(i)
+
+# get S and E coordinates
+#print("S: ", S_xcorr, S_ycorr)
+#print("E: ", E_xcorr, E_ycorr)
