fa14c8f005db84e36bb872c6475998031750d350
Jem Geronimo <jemgeronimo17@gmail.com>
2022-12-14 23:57:33 +0800

    day 12v3 part 2: correct
    Signed-off-by: Jem Geronimo <jemgeronimo17@gmail.com>

---
+++ day12/day12_v3_p2.py
@@ -0,0 +1,95 @@
+SAMPLE_INPUT_ON = 0 # if 1, code is for SAMPLE_INPUT_ON, else, final
+if SAMPLE_INPUT_ON:
+    f = open("sample.txt", "r")
+else:
+    f = open("input.txt", "r")
+
+hmap = []
+for line in f:
+    hmap.append([ord(ch) for ch in line.strip()])
+    #print(line)
+
+    for i in hmap[-1]:
+        if i == ord("S"):
+            S_xcorr = hmap[-1].index(i)
+            S_ycorr = len(hmap) - 1
+            #print(S_xcorr, S_ycorr)
+            hmap[S_ycorr][S_xcorr] = ord("a")
+        elif i == ord("E"):
+            E_xcorr = hmap[-1].index(i)
+            E_ycorr = len(hmap) - 1
+            hmap[E_ycorr][E_xcorr] = ord("z")
+f.close()
+
+#for i in hmap:
+#    print(i)
+class Cell:
+    def __init__(self, x_corr, y_corr):
+        self.x_corr = x_corr
+        self.y_corr = y_corr
+
+# get S and E coordinates
+#print("S: ", S_xcorr, S_ycorr)
+#print("E: ", E_xcorr, E_ycorr)
+
+NUM_ROWS = len(hmap)
+NUM_COLS = len(hmap[0])
+
+# create list with heights "a"
+list_height_a = []
+for j in range(len(hmap)):
+    for i in range(len(hmap[j])):
+        if hmap[j][i] == ord("a"):
+            index = [i, j]
+            list_height_a.append(index)
+            #print("\"coordinates with 'a': ", index)
+print("list_height_a: ", list_height_a)
+
+q1 = [[E_xcorr, E_ycorr]]
+q2 = []
+curr_lvl_list = [1]
+#while([E_xcorr, E_ycorr] not in q1):
+while(any(item in q1 for item in list_height_a) == False):
+#for i in range(5):
+    #print("curr_lvl_list: ", curr_lvl_list)
+    curr_lvl = curr_lvl_list[0]
+    print("level: ", curr_lvl)
+    q2.append(q1.pop(0))
+    curr_corr = q2[-1]
+    curr_height = hmap[curr_corr[1]][curr_corr[0]]
+    no_eligible_cell = 0
+
+    # check up
+    if curr_corr[1] - 1 >= 0:   ## is there a cell in that direction?
+        next_corr = [curr_corr[0], curr_corr[1] - 1] ##
+        next_height = hmap[next_corr[1]][next_corr[0]]
+        if  curr_height <= next_height + 1 and next_corr not in q1 and next_corr not in q2: # is that cell eligible? and not yet in q1 and q2
+            q1.append(next_corr)
+            curr_lvl_list.append(curr_lvl+1)
+    # check right
+    if curr_corr[0] + 1 < NUM_COLS:   ## is there a cell in that direction?
+        next_corr = [curr_corr[0] + 1, curr_corr[1]] ##
+        next_height = hmap[next_corr[1]][next_corr[0]]
+        if  curr_height <= next_height + 1 and next_corr not in q1 and next_corr not in q2: # is that cell eligible? and not yet in q1 and q2
+            q1.append(next_corr)
+            curr_lvl_list.append(curr_lvl+1)
+    # check down
+    if curr_corr[1] + 1 < NUM_ROWS:   ## is there a cell in that direction?
+        next_corr = [curr_corr[0], curr_corr[1] + 1] ##
+        next_height = hmap[next_corr[1]][next_corr[0]]
+        if curr_height <= next_height + 1 and next_corr not in q1 and next_corr not in q2: # is that cell eligible? and not yet in q1 and q2
+            q1.append(next_corr)
+            curr_lvl_list.append(curr_lvl+1)
+    # check left
+    if curr_corr[0] - 1 >= 0:   ## is there a cell in that direction?
+        next_corr = [curr_corr[0] - 1, curr_corr[1]] ##
+        next_height = hmap[next_corr[1]][next_corr[0]]
+        if curr_height <= next_height + 1 and next_corr not in q1 and next_corr not in q2: # is that cell eligible? and not yet in q1 and q2
+            q1.append(next_corr)
+            curr_lvl_list.append(curr_lvl+1)
+    #print("q1: ", q1)
+    #print("q2: ", q2)
+    curr_lvl_list.pop(0)
+
+#print("S: ", S_xcorr, S_ycorr)
+print("E: ", E_xcorr, E_ycorr)
