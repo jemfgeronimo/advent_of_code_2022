983c06204ac06d15a2279998bfe1a6df5e6531fb
Jem Geronimo <Johnerasmusmari.Geronimo@analog.com>
2022-12-15 11:53:50 +0800

    day 15: initial files
    Signed-off-by: Jem Geronimo <Johnerasmusmari.Geronimo@analog.com>

---
+++ day15/day15.py
@@ -0,0 +1,6 @@
+SAMPLE_INPUT_ON = 1 # if 1, code is for SAMPLE_INPUT_ON, else, final
+if SAMPLE_INPUT_ON:
+    f = open("sample.txt", "r")
+else:
+    f = open("input.txt", "r")
+f.close()
---
+++ day15/scratch.py
@@ -0,0 +1,4 @@
+if any(x == "jem" for x in ["e", "jem"]):
+    print("here")
+else:
+    print("missing")
