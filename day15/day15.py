0bb8dd822100aceff704aeb102996fed2b683221
Jem Geronimo <Johnerasmusmari.Geronimo@analog.com>
2022-12-15 20:26:51 +0800

    day 15 part 1: how large should be my matrix?
    Signed-off-by: Jem Geronimo <Johnerasmusmari.Geronimo@analog.com>

--- day15/day15.py
+++ day15/day15.py
@@ -5,10 +5,42 @@ else:
     f = open("input.txt", "r")
 
 # parse input to easier input format
-#for line in f:
-for i in range(1):
-    line = line.strip()
-    line = line.split()
-    
+# + extract min and max values of virtual plane
+# + extract size of virtual plane
+xmin = None
+xmax = None
+ymin = None
+ymax = None
+input = []
+for line in f:
+    # convert string line to list of 4 integers [sensor_x, sensor_y, beacon_x, beacon_y]
+    line = line.strip().split()
+    line = [line[2].strip(","), line[3].strip(":"), line[-2].strip(","), line[-1]]
+    line = [int(str[2:]) for str in line]
 
+    # min/max values of plane
+    if xmin == None:
+        xmin = min(line[0], line[2])
+    else:
+        xmin = min(line[0], line[2], xmin)
+    if xmax == None:
+        xmax = max(line[0], line[2])
+    else:
+        xmax = max(line[0], line[2], xmax)
+    if ymin == None:
+        ymin = min(line[1], line[3])
+    else:
+        ymin = min(line[1], line[3], ymin)
+    if ymax == None:
+        ymax = max(line[1], line[3])
+    else:
+        ymax = max(line[1], line[3], ymax)
+
+    input.append(line)
 f.close()
+# size of virtual plane
+NUM_COLS = xmax - min(xmin, 0)
+NUM_ROWS = ymax - min(ymin, 0)
+print(NUM_COLS, NUM_ROWS)
+print(ymin)
+#print(input)
--- day15/scratch.py
+++ day15/scratch.py
@@ -1,4 +1,8 @@
-if any(x == "jem" for x in ["e", "jem"]):
-    print("here")
-else:
-    print("missing")
+line = "Sensor at x=2, y=18: closest beacon is at x=-2, y=15\n"
+print(line)
+
+# convert string line to list of 4 integers [sensor_x, sensor_y, beacon_x, beacon_y]
+line = line.strip().split()
+line = [line[2].strip(","), line[3].strip(":"), line[-2].strip(","), line[-1]]
+line = [int(str[2:]) for str in line]
+print(line)
