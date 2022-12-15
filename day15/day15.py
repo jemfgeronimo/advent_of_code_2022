e76ab0a0cd68b1c4d0f6f58e9797eb0f7cc1e900
Jem Geronimo <Johnerasmusmari.Geronimo@analog.com>
2022-12-15 18:23:47 +0800

    day 15: added files
    Signed-off-by: Jem Geronimo <Johnerasmusmari.Geronimo@analog.com>

--- day15/day15.py
+++ day15/day15.py
@@ -3,4 +3,12 @@ if SAMPLE_INPUT_ON:
     f = open("sample.txt", "r")
 else:
     f = open("input.txt", "r")
+
+# parse input to easier input format
+#for line in f:
+for i in range(1):
+    line = line.strip()
+    line = line.split()
+    
+
 f.close()
